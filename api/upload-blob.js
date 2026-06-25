5e61e332641b25e829b84746cd3250cf23357303
sakshampokharel01-beep <sakshampokharel01@gmail.com>
2026-06-25 18:07:20 +0545

    Remove access parameter to use store default (private)

--- api/upload-blob.js
+++ api/upload-blob.js
@@ -24,7 +24,6 @@ export default async function handler(req, res) {
 
   try {
     const blob = await put(pathname, req, {
-      access: 'public',
       contentType: contentType || 'application/octet-stream',
       addRandomSuffix: false,
     });
